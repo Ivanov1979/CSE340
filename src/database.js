0be63c40de711108ca79cc19d1a3087e400aa51f
Ivanov1979 <isanhuezaantris@byupathway.edu>
2026-03-26 20:49:50 -0300

    Fix database connection for production

--- src/database.js
+++ src/database.js
@@ -2,10 +2,10 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const db = new Pool({
-    user: "ivanov",
-    host: "/tmp",
-    database: "cse340",
-    port: 5432
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export default db;
