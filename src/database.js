2f8d6f1d1107d9c15eadac92172fde1059e90879
Ivanov1979 <isanhuezaantris@byupathway.edu>
2026-03-26 21:05:40 -0300

    Update database config for local and production

--- src/database.js
+++ src/database.js
@@ -1,11 +1,18 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-const db = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
+const db = new Pool(
+    process.env.DATABASE_URL
+        ? {
+            connectionString: process.env.DATABASE_URL,
+            ssl: { rejectUnauthorized: false }
+        }
+        : {
+            user: "ivanov",
+            host: "/tmp",
+            database: "cse340",
+            port: 5432
+        }
+);
 
 export default db;
