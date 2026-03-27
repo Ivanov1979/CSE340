cae8efeb003160aa977b0aaa8885be3dde30ee53
Ivanov1979 <isanhuezaantris@byupathway.edu>
2026-03-26 22:30:17 -0300

    Support DATABASE_URL and DB_URL in production

--- src/database.js
+++ src/database.js
@@ -1,18 +1,18 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-const db = new Pool(
-    process.env.DATABASE_URL
-        ? {
-            connectionString: process.env.DATABASE_URL,
-            ssl: { rejectUnauthorized: false }
-        }
-        : {
-            user: "ivanov",
-            host: "/tmp",
-            database: "cse340",
-            port: 5432
-        }
-);
+const connectionString = process.env.DATABASE_URL || process.env.DB_URL;
+
+const db = connectionString
+    ? new Pool({
+        connectionString,
+        ssl: { rejectUnauthorized: false }
+    })
+    : new Pool({
+        user: "ivanov",
+        host: "/tmp",
+        database: "cse340",
+        port: 5432
+    });
 
 export default db;
